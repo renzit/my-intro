62a8d131bd9e3edcf3c5c3761275be257014b263
Renzo <renzo.mayer@vix.com>
2020-08-06 04:21:13 -0300

    add more info and metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,12 @@
 module.exports = {
+  siteMetadata: {
+    description: "¿Estás trabajando en algo genial? ¡Me encantaría ayudar a que esto suceda! ¡Envíame un email y comienza tu proyecto ahora mismo! Simplemente hazlo.",
+    locale: "es",
+    title: "Renzo Mayer",
+  },
     plugins: [
       {
-        resolve: "@wkocjan/gatsby-theme-intro",
+        resolve: "my-intro",
         options: {
           theme: "classic",
         },
