65a82669bb5a33cafc5dd8b69b9c4d398ef0bfc7
Renzo <renzo.mayer@vix.com>
2020-08-06 05:48:05 -0300

    missing text

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: "Renzo Mayer - Desarrollador Web, Fotógrafo & Filmmaker",
-    description: "¿Estás trabajando genial? ¡Me encantaría ayudarte! ¡Envíame un correo y comenzá tu proyecto ahora mismo!",
+    description: "¿Estás trabajando en algo genial? ¡Me encantaría ayudarte! ¡Envíame un correo y comenzá tu proyecto ahora mismo!",
     locale: "es",
   },
     plugins: [
