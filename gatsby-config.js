7e5fbcdb9f3e0a4d1e3c1f89b84a4fa60286ae1d
Renzo <renzo.mayer@vix.com>
2020-08-07 17:15:05 -0300

    add analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,13 +4,20 @@ module.exports = {
     description: "¿Estás trabajando en algo genial? ¡Me encantaría ayudarte! ¡Envíame un correo y comenzá tu proyecto ahora mismo!",
     locale: "es",
   },
-    plugins: [
-      {
-        resolve: "@wkocjan/gatsby-theme-intro",
-        options: {
-          showThemeLogo: false,
-          theme: "classic",
-        },
+  plugins: [
+    {
+      resolve: "@wkocjan/gatsby-theme-intro",
+      options: {
+        showThemeLogo: false,
+        theme: "classic",
       },
-    ],
+    },
+  
+  {
+    resolve: "gatsby-plugin-google-tagmanager",
+    options: {
+      id: "UA-174922659-1",
+    },
   }
+  ],
+}
