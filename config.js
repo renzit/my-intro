88a2651ffa8fa8338cb00bfb06e3331ccd9483f8
Renzo <renzo.mayer@vix.com>
2020-08-09 21:46:42 -0300

    add icons

--- config.js
+++ config.js
@@ -1,8 +1,11 @@
 module.exports = {
     siteTitle: "Renzo Mayer - Desarrollador Web, Fotógrafo & Filmmaker",
+    siteTitleAlt: "Renzo Mayer", // Alternative site title for SEO.
     siteLocale: "es",
     siteDescription:
     "¿Estás trabajando en algo genial? ¡Me encantaría ayudarte! ¡Envíame un correo y comenzá tu proyecto ahora mismo!",
     googleTagManagerID: "GTM-MTQ4W7R",
+    backgroundColor: "#ffffff",
+    themeColor: "#e2e2e2",
   };
   
--- gatsby-config.js
+++ gatsby-config.js
@@ -14,12 +14,34 @@ module.exports = {
         theme: "classic",
       },
     },
-  
-  {
-    resolve: "gatsby-plugin-google-tagmanager",
-    options: {
-      id: config.googleTagManagerID,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: config.siteTitle,
+        short_name: config.siteTitleAlt,
+        start_url: '/',
+        background_color: config.backgroundColor,
+        theme_color: config.themeColor,
+        display: 'standalone',
+        icons: [
+          {
+            src: `/icons/icon-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/icons/icon-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
     },
-  }
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: config.googleTagManagerID,
+      },
+    }
   ],
 }
